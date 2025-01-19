d13e262ba7d0b8280d0c89965f7668804be65276
digambar342 <digambar.angul@gmail.com>
2025-01-19 18:41:19 +0530

    commit 2

--- src/main/java/pages/SigninEmailMobile.java
+++ src/main/java/pages/SigninEmailMobile.java
@@ -33,7 +33,7 @@ public class SigninEmailMobile{
 	
 	public SigninPassword clickContinue() {
 		continueButton.click();
-		System.out.println("commit 1");
+		System.out.println("commit 2");
 		return new SigninPassword(driver);
 		
 	}
