a3b69ab9f49a547bacaadbca247561f67cd546c1
digambar342 <digambar.angul@gmail.com>
2025-01-19 15:33:38 +0530

    Revert "revert "Amazon commit""
    This reverts commit dc5f3e51545cbdbedbb1c243f1a5e986ee02d2c7.

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
@@ -16,8 +17,10 @@ public class BaseTest {
 	public WebDriver driver;
 	@BeforeMethod
 	public void setup(ITestContext context) throws IOException {
+		ChromeOptions options= new ChromeOptions();
+		options.addArguments("--headless");
 		System.setProperty("webdriver.chrome.driver",Config.getChromeDriverPath());
-		driver= new ChromeDriver();
+		driver= new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get(Config.getURL());
